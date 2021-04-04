1ab7fe0815b434e3e2e3284275952f09950c8aab
RakeshPatil111 <rakesh.patil4471@gmail.com>
2021-04-04 16:37:36 +0530

    Added - Custom Hilt Test

--- app/src/androidTest/java/com/example/proficiencytest/repository/db/FactDAOTest.kt
+++ app/src/androidTest/java/com/example/proficiencytest/repository/db/FactDAOTest.kt
@@ -5,9 +5,12 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.example.proficiencytest.HiltTestRunner
 import com.example.proficiencytest.getOrAwaitValue
 import com.example.proficiencytest.model.response.Row
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.After
@@ -15,24 +18,27 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class FactDAOTest {
+    @get:Rule
+    var hiltRunner = HiltAndroidRule(this)
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
-    private lateinit var factDatabase: FactDatabase
+
+    @Inject
+    @Named("test_db")
+    lateinit var factDatabase: FactDatabase
     private lateinit var factDAO: FactDAO
 
     @Before
     fun setup() {
-        factDatabase = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            FactDatabase::class.java
-        ).allowMainThreadQueries().build()
-
+        hiltRunner.inject()
         factDAO = factDatabase.getFactDao()
     }
 
@@ -53,9 +59,9 @@ class FactDAOTest {
 
     @Test
     fun testGetAllFacts() = runBlocking {
-        var fact1 = Row(description = "sample description", imageHref = null, title = "Sample")
-        var fact2 = Row(description = "sample description", imageHref = null, title = "Sample")
-        var fact3 = Row(description = "sample description", imageHref = null, title = "Sample")
+        var fact1 = Row(description = "sample description", imageHref = null, title = "Sample1")
+        var fact2 = Row(description = "sample description", imageHref = null, title = "Sample2")
+        var fact3 = Row(description = "sample description", imageHref = null, title = "Sample3")
         factDAO.insertAllFacts(listOf(fact1, fact2, fact3))
 
         val facts = factDAO.getAllFacts().getOrAwaitValue()
